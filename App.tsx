2d6bed2ad6b16122cfecb8fafd0555c8285b97c1
LN42352339 <LN42352339@idat.pe>
2024-12-30 15:43:27 -0500

    Se realizo el front  del login y agrego imagenes

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 import {
   TextInput,
   Text,
@@ -10,18 +11,21 @@ import {
 } from 'react-native';
 
 import Svg, {Path} from 'react-native-svg';
+import {Mycolors} from './src/presentation/theme/AppTheme';
 
 function App(): React.JSX.Element {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <View style={styles.container}>
       {/* Barra de estado con fondo azul */}
-      <StatusBar backgroundColor="#000b76" barStyle="light-content" />
+      <StatusBar backgroundColor={Mycolors.primary} barStyle="light-content" />
 
       {/* Fondo azul y SVG */}
       <View style={styles.headerBackground}>
         <Svg viewBox="0 0 1440 320" style={styles.svg}>
           <Path
-            fill="#000b76"
+            fill={Mycolors.primary}
             fillOpacity="1"
             d="M0,288L34.3,272C68.6,256,137,224,206,224C274.3,224,343,256,411,266.7C480,277,549,267,617,224C685.7,181,754,107,823,85.3C891.4,64,960,96,1029,144C1097.1,192,1166,256,1234,261.3C1302.9,267,1371,213,1406,186.7L1440,160L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z"
           />
@@ -35,24 +39,40 @@ function App(): React.JSX.Element {
           style={styles.image}
         />
         <Image
+          source={require('./assets/img/teladearana.png')}
+          style={styles.image3}
+        />
+        <Image
           source={require('./assets/img/marvellogin.png')}
           style={styles.image2}
         />
         <TextInput
+          style={styles.textInput}
+          placeholderTextColor={Mycolors.placeholder}
           placeholder="Correo Electrónico"
-          style={styles.textinput}
-          placeholderTextColor="rgba(0, 0, 0, 0.2)"
+          value={email} // Vincula el estado
+          onChangeText={setEmail} // Actualiza el estado al escribir
+          onFocus={() => setEmail('')} // Limpia el campo al enfocarse
         />
         <TextInput
+          style={styles.textInput}
           placeholder="Contraseña"
-          style={styles.textinput}
-          placeholderTextColor="rgba(0, 0, 0, 0.2)"
+          placeholderTextColor={Mycolors.placeholder}
           secureTextEntry={true}
+          value={password} // Vincula el estado
+          onChangeText={setPassword} // Actualiza el estado al escribir
+          onFocus={() => setPassword('')} // Limpia el campo al enfocarse
         />
         <TouchableOpacity style={styles.button} onPress={() => {}}>
-          <Text style={styles.buttontext}>Inicia sesión</Text>
+          <View></View>
+          <Text style={styles.buttonText}>Inicia sesión</Text>
+          <Image
+            source={require('./assets/img/escudo.png')}
+            // camerica.png icohulk aranaico
+            style={styles.buttonIcon}
+          />
         </TouchableOpacity>
-        <Text style={styles.textregister}>REGISTRATE AHORA</Text>
+        <Text style={styles.textRegister}>REGISTRATE AHORA</Text>
       </View>
     </View>
   );
@@ -61,10 +81,10 @@ function App(): React.JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white', //del contenerdor principal
+    backgroundColor: Mycolors.background,
   },
   headerBackground: {
-    backgroundColor: '#000b76', //logo marvel
+    backgroundColor: Mycolors.primary, //logo marvel
     width: '100%',
     height: 200, // Ajusta la altura según sea necesario
     position: 'absolute',
@@ -80,12 +100,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 10,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#fff',
-    marginBottom: 20,
-  },
+
   image: {
     height: 50,
     width: 350,
@@ -100,35 +115,52 @@ const styles = StyleSheet.create({
     width: 125,
     zIndex: 10, // Asegura que esté encima de otros elementos
   },
-  textinput: {
-    width: '80%',
-    borderBottomColor: '#000',
+  image3: {
+    position: 'absolute',
+    top: 50,
+    height: 350,
+    width: 500,
+    zIndex: 10, // Asegura que esté encima de otros elementos
+  },
+
+  textInput: {
+    width: '90%',
+    borderBottomColor: Mycolors.placeholder,
     borderBottomWidth: 1,
-    fontSize: 15,
+    fontSize: 12,
     marginBottom: 20,
-    padding: 5,
+    padding: 2,
+    color: Mycolors.secondary,
   },
   button: {
-    width: '80%',
-    backgroundColor: 'red',
+    width: '90%',
+    backgroundColor: '#3B4480',
     height: 50,
     marginTop: 20,
     alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 6,
+    justifyContent: 'space-around',
+    borderRadius: 8,
+    flexDirection: 'row',
   },
-  buttontext: {
+  buttonIcon: {
+    height: 40,
+    width: 40,
+    marginLeft: -140,
+  },
+  buttonText: {
     fontSize: 15,
     color: 'white',
-    textAlign: 'center',
+    textAlign: 'left',
     width: '100%',
+    marginLeft: 60,
   },
-  textregister: {
+  textRegister: {
     width: '100%',
     fontSize: 9,
     textAlign: 'center',
     marginTop: 20,
     marginBottom: 60,
+    color: Mycolors.secondary,
   },
 });
 
---
+++ src/presentation/theme/AppTheme.tsx
@@ -0,0 +1,6 @@
+export const Mycolors = {
+  primary: '#2586DE',
+  background: '#E53935',
+  secondary: '#F5F5F5',
+  placeholder: '#FAFAFA',
+};
