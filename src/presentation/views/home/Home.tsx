12629119d4f46bfe246abb412d061ad076b87f6f
LN42352339 <LN42352339@idat.pe>
2024-12-30 21:41:23 -0500

    Se crea un archivo styles para poder separar de forma independendiente asi pueda quedar mas limpio

--- src/presentation/views/home/Home.tsx
+++ src/presentation/views/home/Home.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import {StackScreenProps} from '@react-navigation/stack';
 import {RootStackParamList} from '../../navigation/MainStackNavigator';
-import {
-  Text,
-  View,
-  Image,
-  StyleSheet,
-  StatusBar,
-  TouchableOpacity,
-} from 'react-native';
+import {Text, View, Image, StatusBar, TouchableOpacity} from 'react-native';
 import {Mycolors} from '../../theme/AppTheme';
 import Svg, {Path} from 'react-native-svg';
 import {DefaultTextInput} from '../../components/DefaultTextInput';
 import {DefaultButton} from '../../components/DefaultButton';
+import styles from './Styles';
 
 interface Props extends StackScreenProps<RootStackParamList, 'HomeScreen'> {}
 
@@ -69,58 +63,3 @@ export const HomeScreen = ({navigation, route}: Props) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Mycolors.background,
-  },
-  headerBackground: {
-    backgroundColor: Mycolors.primary, //logo marvel
-    width: '100%',
-    height: 200, // Ajusta la altura según sea necesario
-    position: 'absolute',
-    top: 0,
-    zIndex: -1,
-  },
-  svg: {
-    padding: 250,
-  },
-  content: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 10,
-  },
-
-  image: {
-    height: 50,
-    width: 370,
-    marginBottom: 250,
-    padding: 80,
-  },
-
-  image2: {
-    position: 'absolute',
-    top: 50,
-    height: 220,
-    width: 125,
-    zIndex: 10, // Asegura que esté encima de otros elementos
-  },
-  image3: {
-    position: 'absolute',
-    top: 50,
-    height: 350,
-    width: 500,
-    zIndex: 10, // Asegura que esté encima de otros elementos
-  },
-
-  textRegister: {
-    width: '100%',
-    fontSize: 9,
-    textAlign: 'center',
-    marginTop: 20,
-    marginBottom: 60,
-    color: Mycolors.secondary,
-  },
-});
